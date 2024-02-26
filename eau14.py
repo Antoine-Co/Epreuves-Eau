9fb095caafffffa10b77c6eddd09155e0b93730e
AntoineCo <an.cornilleau@laposte.net>
2024-02-26 16:37:15 +0100

    Finalisation de la mise en forme fonction, display, arguments

--- eau14.py
+++ eau14.py
@@ -7,3 +7,18 @@ arguments = sys.argv[1:]
 #Check if the arguments are OK
 if (is_args_len_inf(arguments, 2) or is_args_all_num(arguments)):
     error_case()
+
+#By the same algo than bubble sort
+def tri_ascii(tab):
+    n = len(tab)
+
+    #Explore the list
+    for i in range(n):
+        for j in range(0, n-i-1): #For the index that goes until the pre ultime value
+            if (ord(tab[j][0]) > ord(tab[j+1][0])): #get th ord for the ascii order
+                tab[j], tab[j+1] = tab[j+1], tab[j] #Replace the values in the tab
+
+    return tab
+
+#Display the result
+print(tri_ascii(arguments))
